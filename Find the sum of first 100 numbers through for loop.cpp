8fc087b40a4084b19c2a287a9bcbda605a962828
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 20:21:49 +0500

    Update and rename Source1.cpp to Find the sum of first 100 numbers through for loop.cpp

---
+++ Find the sum of first 100 numbers through for loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"\n**************************************************************************";
+cout<<"\nProgrammer :  Sohail Zia Gill.";
+cout<<"\nTitle: It calculate the sum of first 100 numbers.";
+cout<<"\nCompiler : Visual Studio 19";
+cout<<"\nDated : 1/Oct/19";
+cout<<"\nCompiling date : 1/Oct/19";
+cout<<"\nIslamic International University Islamabad.";
+cout<<"\n***************************************************************************";
+	int f1=0,sum=0;
+	for(f1;f1<100;f1++)
+	{
+		sum=sum+f1;
+	
+	}
+	cout<<"\n The sum of first 100 numbers are   "<<sum;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
