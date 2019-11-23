6e6fb5b1efa1ea89dd497dce809a9782c38f81ba
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 20:20:29 +0500

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int f1=0,sum=0;
+	for(f1;f1<100;f1++)
+	{
+		sum=sum+f1;
+	
+	}
+	cout<<"\n The sum of first 100 numbers are   "<<sum;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
