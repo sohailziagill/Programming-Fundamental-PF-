854337354005e9eacd2df632d64b894670a772f7
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 22:17:50 +0500

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main ()
+ {
+cout<<"CCCCCCCCCCc"<<setw(15)<<"++"<<setw(20)<<"++"<<endl;
+cout<<"cc"<<setw(24)<<"++"<<setw(20)<<"++"<<endl;
+cout<<"cc"<<setw(30)<<"+++++++++++++"<<setw(20)<<"++++++++++++"<<endl;
+cout<<"cc"<<setw(30)<<"+++++++++++++"<<setw(20)<<"++++++++++++"<<endl;
+cout<<"cc"<<setw(24)<<"++"<<setw(20)<<"++"<<endl;
+
+cout<<"CCCCCCCCCCc"<<setw(15)<<"++"<<setw(20)<<"++"<<endl;
+
+   return 0;
+}
