236059c5d8b686d7b88c6aff3b2d72d005b9614a
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 20:24:56 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int f1=0,f2=1,f3;
+	int value,count=2;
+	f3=f2+f1;
+	cout<<"Please enter the value  ";
+	cin>>value;
+	if(value==1)
+		cout<<f1;
+	 if(value==2)
+		 cout<<f2;
+	 if(value>2)
+	 {
+	while(count<value)
+	{
+		f1=f2;
+		f2=f3;
+		f3=f2+f1;
+		count++;
+		cout<<"\t"<<f3;
+	}
+	
+	
+	 }
+	 cout<<endl;
+	 
+	system("pause");
+	return 0;
+}
