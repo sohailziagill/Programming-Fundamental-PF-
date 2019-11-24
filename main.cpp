a9a97b4464f192cc63f4d1cf3bfef19cac8d62f3
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-24 11:12:56 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,sum;
+    cout<<"Enter the number ";
+    cin>>num;
+    while(num!=0)
+    {
+        sum=sum+num%10;
+        num=num/10;
+        
+        
+        
+    }
+    if(sum%3==0)
+    cout<<"Yes it is divisible by  3";
+    if(sum%9==0)
+    cout<<"\nYes it is divisible  by 9 ";
+    
+    
+    
+return 0;
+
+}
